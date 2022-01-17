5c2872f7a9ddb2e400eecb42165d16f4e5228575
Mozetsu <code.moz.00@gmail.com>
2022-01-17 17:30:32 +0000

    Update login page.

--- pages/login.js
+++ pages/login.js
@@ -2,18 +2,22 @@ import { getProviders, signIn } from 'next-auth/react';
 
 function Login({ providers }) {
 	return (
-		<div>
+		<div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
 			<img
-				className="w-100 mb-5"
+				className="h-12 mb-10"
 				src="https://raw.githubusercontent.com/Mozetsu/react-spotify/main/public/spotify.svg"
 				alt=""
 			></img>
 			{Object.values(providers).map((provider) => (
-				<div>
-					<button>Login with {provider.name}</button>
+				<div key={provider.name}>
+					<button
+						onClick={() => signIn(provider.id, { callbackUrl: '/' })}
+						className="bg-[#18D860] text-white p-5 rounded-full"
+					>
+						Login with {provider.name}
+					</button>
 				</div>
 			))}
-			console.log(providers);
 		</div>
 	);
 }
