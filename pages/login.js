6f41add6d7ce39f82ce10b58af1bb8ee0edd7c9a
Mozetsu <code.moz.00@gmail.com>
2022-01-17 17:14:48 +0000

    Fix [...nextauth].js file path.

--- pages/login.js
+++ pages/login.js
@@ -3,7 +3,17 @@ import { getProviders, signIn } from 'next-auth/react';
 function Login({ providers }) {
 	return (
 		<div>
-			<h1>Login page</h1>
+			<img
+				className="w-100 mb-5"
+				src="https://raw.githubusercontent.com/Mozetsu/react-spotify/main/public/spotify.svg"
+				alt=""
+			></img>
+			{Object.values(providers).map((provider) => (
+				<div>
+					<button>Login with {provider.name}</button>
+				</div>
+			))}
+			console.log(providers);
 		</div>
 	);
 }
