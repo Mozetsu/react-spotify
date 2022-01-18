125842ff5f1f5aa4bfcd7f7f7004c075cc81045b
Mozetsu <code.moz.00@gmail.com>
2022-01-18 00:39:14 +0000

    Built songs components.

--- components/Center.js
+++ components/Center.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { playlistState, playlistIdState } from '../atoms/playlistAtom';
 import spotifyApi from '../lib/spotify';
+import Songs from './Songs';
 
 const colors = [
 	'from-indigo-500',
@@ -36,20 +37,31 @@ function Center() {
 				console.log(error);
 			});
 	}, [spotifyApi, playlistId]);
-	console.log(playlist);
+	// console.log(playlist);
 	return (
-		<div className="flex-grow text-white">
+		<div className="flex-grow text-white h-screen overflow-y-scroll scrollbar-hide">
 			<header className="absolute top-5 right-8">
-				<div className="flex items-center space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 bg-black">
+				<div
+					onClick={signOut}
+					className="flex items-center space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 bg-black"
+				>
 					<img className="rounded-full w-10 h-10" src={session?.user.image} alt=""></img>
 					<h2>{session?.user.name}</h2>
 					<ChevronDownIcon className="h-5 w-5"></ChevronDownIcon>
 				</div>
 			</header>
-			{/* playlist.tracks */}
+			{/* playlist header */}
 			<section className={`flex items-end space-x-7 bg-gradient-to-b ${color} to-black h-80 text-white p-8`}>
-				{/* <img className="h-44 w-44 shadow-2xl" src={playlist.images[0].url} alt=""></img> */}
+				<img className="h-44 w-44 shadow-2xl" src={playlist?.images?.[0]?.url} alt=""></img>
+				<div>
+					<p>PLAYLIST</p>
+					<h1 className="text-2xl md:text-3xl xl:text-5xl font-bold">{playlist?.name}</h1>
+				</div>
 			</section>
+			{/* playlist tracks */}
+			<div>
+				<Songs />
+			</div>
 		</div>
 	);
 }
